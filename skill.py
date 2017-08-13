618a7f73d04937cd7fe585635004e0394193d8b1
Matthew Villegas <matthew.villegas@knights.ucf.edu>
2017-08-13 17:40:22 -0400

    Comment additions

--- skill.py
+++ skill.py
@@ -14,27 +14,35 @@ ask = Ask(app, "/")
 
 
 def getGarageInfo():
-
+	# downloads the HTML from the UCF Parking Garage page
 	page = requests.get('http://secure.parking.ucf.edu/GarageCount/iframe.aspx')
-
+	
+	# turn the HTML into a BeautifulSoup object to parse
 	soup = BeautifulSoup(page.content, 'html.parser')
-
+	
+	# separates all javascript scripts into a list to iterate through later
 	newlist = list(soup.find_all('script', type="text/javascript"))
-	i = 0
+	
+	i = 0 # need to fix this so I can just enumerate the for each loop
 	garageNames = ['A', 'B', 'C', 'D', 'H', 'I', 'Libra']
 	percentages = []
 	message = ''
+	
+	# loop through each script in the list and find the percentages in the script
 	for item in newlist:
 		if('percent:' in item.get_text()):
-			percent = re.findall('\d+', item.get_text())[1]
+			# some regex for getting the percentage as text
+			percent = re.findall('\d+', item.get_text())[1] 
+			# output formatting
 			if(garageNames[i] == "Libra"):
 				print(garageNames[i] +" garage" +" is at " +percent+"% capacity")
+				# concatenates the print into a new string to return as a statement for Alexa
 				message = message + "<s>" + garageNames[i] +" garage" +" is at " +percent+" percent capacity </s>"
 			else:
 				print("Garage " + garageNames[i]+ " is at " + percent+"percent capacity")
 				message = message + "<s>Garage " + garageNames[i]+ " is at " + percent+" percent capacity</s>"
 			percentages.insert(i, percent)
-			i+=1
+			i+=1 # still need to fix this using enumerate
 
 
 	return statement("<speak>"+message+"</speak>")
