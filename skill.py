e0c1acde0d515f5cd9a2c9200ad72b006165443a
Matthew Villegas <matthew.villegas@knights.ucf.edu>
2017-08-14 20:37:55 -0400

    comment changes, new intents, method return change
    Changed getGarageInfo() method so that instead of immediately returning the dictionary, it returns the two lists created in the method. 
    
    Changed the Garage intent so that now users can ask for a specific garage's capacity, and added a method two zip the two lists from getGarageInfo() into a dictionary for this method. 
    
    Added a method for the AllGarages intent which is called when a user wants to see the capacities for all of the garages, which is originally what the script did.

--- skill.py
+++ skill.py
@@ -4,53 +4,75 @@ import re
 from flask import Flask
 from flask_ask import Ask, statement, question, session
 
-# setting up Flask app
 app = Flask(__name__)
 
 ask = Ask(app, "/")
 
-# launch intent immediately calls the Garage info function so that it is read off on launch
+
 @ask.launch
+def launchMsg():
+	return statement("Hello. Please say the name of the garage")
+
+# parses garage information into two lists
 def getGarageInfo():
-	# downloads the HTML from the UCF Parking Garage page
+	# get HTML from the parking site
 	page = requests.get('http://secure.parking.ucf.edu/GarageCount/iframe.aspx')
-	
-	# turn the HTML into a BeautifulSoup object to parse
+
+	# turn the site into a beautiful soup object
 	soup = BeautifulSoup(page.content, 'html.parser')
-	
-	# separates all javascript scripts into a list to iterate through later
+
+	# create a list of all javascript scripts
 	newlist = list(soup.find_all('script', type="text/javascript"))
-	
-	i = 0 # need to fix this so I can just enumerate the for each loop
+	i = 0 # TODO: fix this so I can loop in a more pythonic way
+
+	# create lists to store names of garages and their percentages
 	garageNames = ['A', 'B', 'C', 'D', 'H', 'I', 'Libra']
 	percentages = []
+
+	# creates string that will be used for the alexa statement
 	message = ''
-	
-	# loop through each script in the list and find the percentages in the script
+
+	# loop through all of the scripts and insert values into the lists
 	for item in newlist:
+
+		# finds where the percentage is stored and uses regex to get the number from that string
+		# the inserts into the list
 		if('percent:' in item.get_text()):
-			# some regex for getting the percentage as text
-			percent = re.findall('\d+', item.get_text())[1] 
-			# output formatting
-			if(garageNames[i] == "Libra"):
-				print(garageNames[i] +" garage" +" is at " +percent+"% capacity")
-				# concatenates the print into a new string to return as a statement for Alexa
-				message = message + "<s>" + garageNames[i] +" garage" +" is at " +percent+" percent capacity </s>"
-			else:
-				print("Garage " + garageNames[i]+ " is at " + percent+"percent capacity")
-				message = message + "<s>Garage " + garageNames[i]+ " is at " + percent+" percent capacity</s>"
+			percent = re.findall('\d+', item.get_text())[1]
 			percentages.insert(i, percent)
-			i+=1 # still need to fix this using enumerate
+			i+=1 # see TODO above
+	return garageNames, percentages
 
+# creates a dictionary from the two lists 
+def createGarageDict(listOne, listTwo):
+	garageDict = dict(zip(listOne,listTwo))
+	return garageDict
 
-	return statement("<speak>"+message+"</speak>")
-
-# ask intents in case they want a repeat of the capacities
+# called when the user asks for a specific garage
 @ask.intent("Garage")
-def Garage():
-	print("Reached Garage Function")
-	return getGarageInfo()
-# for when the user asks for help
+def Garage(GarageName):
+	garageNames, percentages = getGarageInfo()
+	garageDict = createGarageDict(garageNames,percentages)
+
+	# capitalizes user input to make sure the dictionary is indexed properly
+	GarageName = GarageName.capitalize()
+	return statement("Garage {}".format(GarageName) + " is at {}".format(garageDict[GarageName]) + " percent capacity")
+
+# called when the user aks for information for all garages
+@ask.intent("AllGarages")
+def sayAllGarages():
+	garageNames, percentages = getGarageInfo()
+	message = ''
+	for i in range(0,7):
+		message = message + "<s>Garage " + garageNames[i]+ " is at " + percentages[i] +" percent capacity</s>"
+
+	return statement("<speak>" + message + "</speak>")
+
+
+
+	
+
+
 @ask.intent("AMAZON.HelpIntent")
 def help():
 	return statement("This skill tells you how full each of the garages are at UCF, just launching the skill should tell you the capacities")
